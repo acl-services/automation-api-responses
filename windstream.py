fbe5d03dc1bbe2de5c8e3fd40260cd047a7cf8d3
Nandhini_Ramachandran <85650364+nandhini-galvanize@users.noreply.github.com>
2021-08-03 20:01:07 -0700

    Update windstream.py

--- windstream.py
+++ windstream.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # import all the necessary packages
 
 # system defined module
@@ -6,15 +5,6 @@ from anytree import Node, RenderTree, AsciiStyle, PreOrderIter, LevelOrderIter
 from pathlib import Path
 import warnings
 
-=======
-
-#import all the necessary packages
-
-#system defined module
-from anytree import Node, RenderTree, AsciiStyle, PreOrderIter, LevelOrderIter
-from pathlib import Path
-import warnings
->>>>>>> e2a36b2ceac6c7d03b71985e40a635bf0e9c87d5
 warnings.filterwarnings("ignore")
 import json
 import requests
@@ -23,7 +13,6 @@ from pandas import json_normalize
 import pandas as pd
 import logging
 import os
-<<<<<<< HEAD
 
 
 # user-defined module
@@ -79,51 +68,9 @@ def windstream_main():
                     export_to_excel_from_tree.export_to_excel_from_tree(
                         high_bond_root, row.org_id, row.region_code
                     )
-=======
-
-
-#user-defined module
-import tree_structure
-import access_credentials
-import extract_resources_from_api_tree
-import export_to_excel_from_tree
-import windstream_logger
-
-logging= windstream_logger.get_logger("windstream_main")
-
-
-def windstream_main():
-
-    token = input("Enter your access token \n")
-    
-    # error handling for empty token
-    if token == '':
-        
-        logging.critical('Token not provided, exiting the program')
-        exit()
-    
-    # function call for api access credentials
-    else:
-        input_url_list = access_credentials.access_credentials()
-       
-
-        for index, row in input_url_list.iterrows():
-            if 'base_url' in input_url_list.columns: 
-                if row.base_url !='nan' :
-                    
-                    logging.info("the base url is :"+str(row.base_url))
-            
-                    # function call for extract_resources_from_url
-                    high_bond_root = tree_structure.build_tree_for_org(row.org_id)
-                    
-                    list_of_df = extract_resources_from_api_tree.extract_resources_from_api_tree(high_bond_root, str(row.base_url), token)
-                    #export_to_excel(list_of_df)
-                    export_to_excel_from_tree.export_to_excel_from_tree(high_bond_root, row.org_id, row.region_code)
->>>>>>> e2a36b2ceac6c7d03b71985e40a635bf0e9c87d5
             else:
                 continue
 
-windstream_main()
 
 # Call main()
 windstream_main()
